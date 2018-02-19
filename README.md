5b12a4a77fdb487f113eb7758d1b066836134e95
vamsi <vamsi.raj11@gmail.com>
2018-02-19 14:34:49 -0600

    test commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# 44-563 A01 Assignment
+
+Learning JavaScript and jQuery by interacting with a user. 
+
+# Uses
+- CSS, a style sheet language for styling markup languages such as html
+- BootStrap, a framework for responsive web design 
+- JavaScript, a scripting language for the web
+- HTML, a markup language for web apps
+
+
+# Getting Started
+
+Visual Studio Code is recommended, but you can use Sublime, Notepad++, or any text editor to modify the code. 
+
+- Right-click on M04 folder / "Open with Code".
+- In VS Code, format code with SHIFT + ALT + f and CTRL-S to save.
+
+# Frameworks and Libraries
+
+- JQuery: https://jquery.com/
+- BootStrap: http://getbootstrap.com/
+
+
+# Developer Tools
+
+- VS Code: https://code.visualstudio.com/
+- Git for Windows: https://git-scm.com/downloads
+- TortoiseGit: https://tortoisegit.org/
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    $('.semester').on('change', function () {
+        let semesters = $(this).val()
+        let html = ''
+        for(i=0; i < semesters; i++) {
+            html += `
+            <div class="form-group">
+                <label for="">Semster ${i+1} GPA</label>
+                <input type="text"
+                class="form-control semester_${i+1}" name="semester_${i+1}" id="semester" aria-describedby="helpId" placeholder="Enter GPA for semester ${i+1}">
+            </div>`
+        }
+
+        $('#semesters').val(semesters)
+        html += '<button onclick="calculateGPA()" class="btn btn-primary">Calculate GPA</button>'
+        $('#generatedHtml').html(html)
+    })
+})
+
+function calculateGPA() {
+    let semesters = $('#semesters').val()
+    let totalGpa = 0
+    for(i=0; i < semesters; i++) {
+        if (parseGpa(parseFloat($(`.semester_${i+1}`).val()))) {
+            totalGpa +=  parseGpa(parseFloat($(`.semester_${i+1}`).val()))
+        }
+        else {
+            return
+        }
+        
+    }
+    
+    let text = ''
+    let finalGPA = calculateFinalGPA(totalGpa, semesters)
+    if (finalGPA >= 3.3) {
+        text = "You availed scholarship"
+    }
+    else {
+        text = "You need minimum of 3.3 GPA to avail scholar ship"
+    }
+
+    $('.output').html(`Your Total GPA average for <span class="no-of-semesters">${semesters}</span> semesters is <span class="calculated-gpa">${finalGPA}</span>
+                        <h1 class="scholarship">${text}</h1>
+                    `)
+}
+
+function calculateFinalGPA (totalGpa, semesters) {
+    return Math.round((totalGpa/semesters) * 100) / 100 
+}
+function parseGpa (gpa, test) {
+    if (gpa === '')
+    {
+        throw Error("GPA cannot be empty")
+    }
+    if (gpa === null ) {
+        throw Error("GPA cannot be null")
+    }
+    if (!(gpa >= 0 && gpa <= 4)) {
+        if(test) {
+            throw Error("GPA should be between 0 and 4")
+        }
+        else {
+            alert(`GPA should be between 0 and 4 for semester ${i+1}`)
+            return false
+        }
+    }
+    else {
+        return gpa
+    }
+}
